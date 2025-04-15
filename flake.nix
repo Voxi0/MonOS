6cddde078d05d855631c8a83218fa770bac18646
Voxi0 <alif200099@gmail.com>
2025-04-15 17:09:55 +0100

    Added Nix Flake
    To easily manage the development environment which contains all required
    packages and stuff for development.

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  # Flake description
+  description = "MonOS dev environment";
+
+  # Dependencies
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Actions
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem(system: let
+    includeDir = "./include";
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [ ];
+    };
+  in with pkgs; {
+    devShells.default = mkShell {
+      buildInputs = [ wget nasm qemu libisoburn ];
+      shellHook = '''';
+    };
+  });
+}
